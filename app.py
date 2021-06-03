851b626e2f21e6de13fe5b2cf8d97284d1242bc7
7benson <jknani111@gmail.com>
2021-06-04 03:18:47 +0530

    initialised deployment process

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import nltk
-nltk.download('punkt')
+nltk.download('punkt')  
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 import re
